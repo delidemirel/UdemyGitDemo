c1a96c05de7e5968c380be2c3bff6d635f15ab07
delidemirel <hdeli@outlook.com>
2022-08-07 15:23:24 -0500

    htc

--- GitJavaProject/src/main/GitDemo.java
+++ GitJavaProject/src/main/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("GOODBYE");
+        System.out.println("Hatice");
     }
 }
